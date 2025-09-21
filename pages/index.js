29f1f7f4f76df955392fd4e4a7b0cff4499bc64e
Lucas Ferreira <103146024+LKSFerreira@users.noreply.github.com>
2025-09-21 18:25:25 +0000

    :sparkles: feat: Impactar a vany

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Teste 1</h1>
+    return <img src="https://tse1.mm.bing.net/th/id/OIP.QkkfiuWrmdGTzsiGdbqepQAAAA" alt="Profile Image" width="1023"/>;
 }
 
 export default Home;
