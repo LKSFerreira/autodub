f4558b15a32efae124097ba44be3fd8bbafa0cc5
Lucas Ferreira <lks_ferreira@hotmail.com>
2025-09-26 01:36:40 -0300

    :lips: style: formata o código com ruff, isort e black

--- src/autodub/implementations/mocks/__init__.py
+++ src/autodub/implementations/mocks/__init__.py
@@ -4,11 +4,11 @@ Mocks de implementações para testes do pipeline de dublagem.
 Todos os módulos aqui devem ser determinísticos e documentados em pt-BR.
 """
 
-from .mock_asr import MockASR
-from .mock_tts import MockTTS
-from .mock_embedding import MockEmbedding
-from .mock_vocoder import MockVocoder
 from .ffmpeg_wrapper import FakeFFmpegWrapper
+from .mock_asr import MockASR
+from .mock_embedding import MockEmbedding
+from .mock_tts import MockTTS
+from .mock_vocoder import MockVocoder
 
 __all__ = [
     "MockASR",
