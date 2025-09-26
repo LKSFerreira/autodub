36f920da338fa0c954532e1d52661805cbd28ecc
Lucas Ferreira <lks_ferreira@hotmail.com>
2025-09-26 00:11:59 -0300

    :test_tube: test: adicionar testes unitários para MockVocoder

---
+++ tests/unit/test_mock_vocoder.py
@@ -0,0 +1,8 @@
+from autodub.implementations.mocks.mock_vocoder import MockVocoder
+
+
+def test_sintetizar_from_mel_retorna_bytes():
+    vocoder = MockVocoder()
+    saida = vocoder.sintetizar_from_mel([[0.1, 0.2], [0.3, 0.4]])
+    assert isinstance(saida, bytes)
+    assert saida == b"FAKE_VOCODER_AUDIO"
